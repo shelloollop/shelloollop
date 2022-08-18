20e0878799cb7ecf32117e03554e409d3b9feef6
shellroollop <gmlw23@naver.com>
2022-08-18 15:22:02 +0900

    ENTER EVENT

--- helloworldcpp/WOW.cpp
+++ helloworldcpp/WOW.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
+    int a;
+
     cout << "New Push" << endl;
     cout << "Update Finish" << endl;
-    cout << "NENENW" << endl;
+    cin >> a;
+    cout << a << endl;
 
     return 0;
 }
