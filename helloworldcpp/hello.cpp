7b533b02d5379dcc04a91556c065929036657edc
shellroollop <gmlw23@naver.com>
2022-08-18 18:28:10 +0900

    NEWWAVE12

--- helloworldcpp/hello.cpp
+++ helloworldcpp/hello.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Hello, Wolrd" << endl;
     cout << "Hello, Jenkins" << endl;
     cout << "Hello, GitHub" << endl;
+    cout << "Hello, Terminal" << endl;
 
     return 0;
 }
