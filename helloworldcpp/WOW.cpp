56d67c243bffc78bd9db8f1ef3ef9138b9bea290
shellroollop <gmlw23@naver.com>
2022-08-18 15:41:11 +0900

    WebhookTest

--- helloworldcpp/WOW.cpp
+++ helloworldcpp/WOW.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    int a, b;
+    int a, b, c;
 
     cout << "New Push" << endl;
     cout << "Update Finish" << endl;
-    cin >> a >> b;
-    cout << a << endl;
+    cin >> a >> b >> c;
+    cout << a << endl << b << endl << c << endl;
 
     return 0;
 }
