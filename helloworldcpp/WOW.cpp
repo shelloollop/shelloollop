448dabe29e7569e2add8487b459fa4d28dab5170
shellroollop <gmlw23@naver.com>
2022-08-18 18:25:12 +0900

    NEWWAVE11

--- helloworldcpp/WOW.cpp
+++ helloworldcpp/WOW.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 int main()
 {
-    int a, b, c, d, e;
+    int a, b, c, d, e, f, g;
 
     cout << "New Push" << endl;
     cout << "Update Finish" << endl;
     cin >> a >> b >> c >> d >> e;
     cout << a << endl << b << endl << c << endl << d << endl << e << endl;
+    f= a + b + c;
+    g = d - e;
+    cout << f << endl << g << endl;
 
     return 0;
 }
--- helloworldcpp/hello.cpp
+++ helloworldcpp/hello.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Hello, Wolrd" << endl;
+    cout << "Hello, Jenkins" << endl;
+    cout << "Hello, GitHub" << endl;
 
     return 0;
 }
