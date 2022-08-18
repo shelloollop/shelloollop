798f67bde48bb18b7579c6dbfddc1865093f10ad
shellroollop <gmlw23@naver.com>
2022-08-18 15:34:53 +0900

    really

--- helloworldcpp/WOW.cpp
+++ helloworldcpp/WOW.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    int a;
+    int a, b;
 
     cout << "New Push" << endl;
     cout << "Update Finish" << endl;
-    cin >> a;
+    cin >> a >> b;
     cout << a << endl;
 
     return 0;
